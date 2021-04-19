6562c4b44efe65dc47bf70aa4dd67a1cb2e14aac
Ahmad Irfaan Hibatullah <irfaan.hibatullah@gmail.com>
2021-04-19 16:39:18 +0700

    Adding system properties

--- src/test/java/com/bookingsroom/irfaan/BookingRoomsWithGraphQlAndRestApiApplicationTests.java
+++ src/test/java/com/bookingsroom/irfaan/BookingRoomsWithGraphQlAndRestApiApplicationTests.java
@@ -1,13 +1,16 @@
 package com.bookingsroom.irfaan;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootTest()
-class BookingRoomsWithGraphQlAndRestApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest()
+//class BookingRoomsWithGraphQlAndRestApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
